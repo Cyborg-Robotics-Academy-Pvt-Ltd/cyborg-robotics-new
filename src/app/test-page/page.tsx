1ab514a66e5a9b71cbfcc70680c1137e2d53bce5
shrikantg199 <shrikantg199@gail.com>
2025-10-30 16:08:21 +0530

    feat(mycomp): restructure components and remove test page

--- src/app/test-page/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="mt-32">
-      <h1>Test Page</h1>
-      <p>This is a simple test page to verify routing works correctly.</p>
-    </div>
-  );
-}
