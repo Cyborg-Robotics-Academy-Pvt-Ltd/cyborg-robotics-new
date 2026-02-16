49d9c34c6e564eb208e202a9f9db87b4143d2c58
shrikantg199 <shrikantg199@gmail.com>
2026-02-16 20:16:22 +0530

    fix(layout): update Google Tag Manager script loading strategy and clean up code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,10 @@ import { ReactNode, Suspense } from "react";
 import ConditionalHeader from "@/components/layout/conditional-header";
 
 import { Poppins } from "next/font/google";
-import { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { WebsiteLoadingProvider } from "@/components/WebsiteLoading";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Script from "next/script";
 
 const poppins = Poppins({
@@ -25,26 +24,26 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" suppressHydrationWarning className={poppins.variable}>
       <head>
         {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TBX67M44');
-          `}
-        </Script>
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBX67M44') `,
+          }}
+        />
         {/* End Google Tag Manager */}
       </head>
+
       <body
         className="font-body antialiased overflow-x-hidden"
         suppressHydrationWarning={true}
       >
-        {/* GTM NoScript */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TBX67M44"
@@ -53,6 +52,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <WebsiteLoadingProvider>
           <AuthProvider>
             <div className="flex min-h-screen w-full flex-col overflow-x-hidden">
@@ -65,7 +66,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             </div>
 
             <Analytics />
-
             <SpeedInsights />
             <Toaster position="top-right" />
           </AuthProvider>
