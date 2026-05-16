916c2b1c9bf76cd8c3d53a3e4159659864bad120
shrikantg199 <shrikantg199@gail.com>
2026-05-16 15:26:03 +0530

    feat(codefest): add index exports for all codefest components
    - Create an index.ts file in the codefest feature folder
    - Export HeroSection component
    - Export FeatureBar component
    - Export AboutChallenge component
    - Export HowItWorks component
    - Export MiniChallenge component
    - Export RegistrationForm component
    - Export Highlights component
    - Export RewardsSection component
    - Export Leaderboard component
    - Export FAQSection component
    - Export FinalCTA component
    - Export Footer component

---
+++ src/components/codefest/index.ts
@@ -0,0 +1,15 @@
+// src/features/codefest/index.ts
+
+
+export { default as HeroSection } from "./HeroSection";
+export { default as FeatureBar } from "./FeaturesBar";
+export { default as AboutChallenge } from "./AboutChallenge";
+export { default as HowItWorks } from "./HowItWorks";
+export { default as MiniChallenge } from "./MiniChallenge";
+export { default as RegistrationForm } from "./RegistrationForm";
+export { default as Highlights } from "./Highlights";
+export { default as RewardsSection } from "./RewardsSection";
+export { default as Leaderboard } from "./Leaderboard";
+export { default as FAQSection } from "./FAQSection";
+export { default as FinalCTA } from "./FinalCTA";
+export { default as Footer } from "./Footer";
